5162a03cbb4bd683bb61b79e3d7ed880a1d29de0
mendesmoe <m.mendes@depiltech.com>
2024-07-16 14:51:06 +0200

    add security with JWT

--- src/main/java/com/postech/mspayment/usecase/CreateInvoiceUseCase.java
+++ src/main/java/com/postech/mspayment/usecase/CreateInvoiceUseCase.java
@@ -2,10 +2,7 @@ package com.postech.mspayment.usecase;
 
 import com.postech.mspayment.entity.Invoice;
 import com.postech.mspayment.entity.InvoiceDTO;
-import com.postech.mspayment.entity.ProductItem;
-import com.postech.mspayment.entity.ProductItemDTO;
 import com.postech.mspayment.repository.InvoiceRepository;
-import com.postech.mspayment.service.CustomerService;
 import com.postech.mspayment.service.PaymentService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,62 +20,20 @@ public class CreateInvoiceUseCase {
     @Autowired
     private InvoiceRepository invoiceRepository;
 
-    @Autowired
-    private CustomerService customerService;
-
-    //@Autowired
-    //    private BasketService basketService;
-
-    public InvoiceDTO execute(Long customerId, Long basketId, BigDecimal amount) throws Exception {
+    public InvoiceDTO execute(Long customerId, Long cartId, BigDecimal amount) throws Exception {
 
         logger.info(" * Creating invoice use case ");
-        // Recupera as informacoes do usuario
-        CustomerService.UserDTO user = customerService.getCustomerById(customerId);
-
-        String customerName = user.getName();
 
         // Cria a Invoice
         Invoice invoice = new Invoice();
         invoice.setCustomerId(customerId);
-        invoice.setBasketId(basketId);
+        invoice.setCartId(cartId);
         invoice.setTotalAmount(amount);
         invoice.setCreatedAt(LocalDateTime.now());
         invoice.setUpdatedAt(LocalDateTime.now());
         Invoice invoiceSaved = invoiceRepository.save(invoice);
 
         return invoiceSaved.toInvoiceDTO();
-
-        //Recupera as informacoes do carrinho
-        //List<ProductItemDTO> products = basketService.getProductsByBasketId(basketId);
-        //// Retrieve basket data
-        //        Basket basket = restTemplate.getForObject("http://localhost:8083/api/baskets/" + basketId, Basket.class);
-        //        if (basket == null) {
-        //            throw new Exception("Error retrieving basket data");
-        //        }
-
-        // Add os ProductItems
-        //for (ProductItemDTO product : products) {
-        //            product.setInvoiceId(invoiceSaved.getId());
-        //
-        //            ProductItem productItem = new ProductItem(product);
-        //
-        //            invoiceSaved.getProducts().add(productItem);
-        //        }
-        //@Data
-        //@RequiredArgsConstructor
-        //public class CartItemDTO {
-        //
-        //    private Long id;
-        //    @NotNull
-        //    private Long productId;
-        //    @Min(value = 1, message = "Quantity must be greater than zero")
-        //    private int quantity;
-        //    @NotNull
-        //    private BigDecimal price;
-        //
-        //}
-
-        //invoiceRepository.save(invoiceSaved);
     }
 }
 
